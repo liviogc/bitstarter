1f0b7162201dea4c60610c8addd1d72ccb5f3ef2
Livio Gibosky Costa <liviogc@gmail.com>
2013-07-07 21:53:06 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require("fs");
+var buffer = new Buffer(fs.readFileSync(__dirname + "index.html", "utf8"), "utf-8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
